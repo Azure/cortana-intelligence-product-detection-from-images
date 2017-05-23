b587e2aedd429f0fde595b3aac9b9aeed49b220c
Lixun Zhang <lxzhang@gmail.com>
2017-05-23 15:26:22 -0400

    Provide sample images for the web site

--- technical_deployment/web_service/WebApp/model.py
+++ technical_deployment/web_service/WebApp/model.py
@@ -11,7 +11,7 @@ import numpy as np
 import cntk
 from PIL import Image, ImageOps
 from io import BytesIO
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, send_from_directory
 from flask_cors import CORS
 import pytz
 from azure.storage.blob import BlockBlobService
@@ -68,9 +68,13 @@ if classifier == "svm":
 else:
     svmWeights, svmBias, svmFeatScale = (None, None, None)
 
+    
 @app.route("/", methods=['GET'])
 def load():
-    return render_template('index.html')
+    target = os.path.join(APP_ROOT, 'images/')
+    image_names = os.listdir(target)
+    return render_template('index.html', image_names=image_names)
+
     
 @app.route("/uploader", methods=['POST'])
 def upload_file():
@@ -91,6 +95,30 @@ def upload_file():
                  modified_date_utc, usage, roi_annotated, roi_annotated_dict, roi_selective_search, model_version)
 
     return render_template('index.html', **locals())
+
+    
+@app.route("/result", methods=['POST'])
+def upload_local_file():
+    target = os.path.join(APP_ROOT, 'images/')
+    image_names = os.listdir(target)
+
+    result = request.form['fileindex']
+    local_image = "/".join([target, result])
+    # save to blob
+    # appliance_id, blob_url, modified_date, modified_date_utc, usage = save_file(file)
+
+    # score
+    # img = Image.open(BytesIO(file.read())).convert('RGB')
+    roi_annotated, roi_annotated_dict, roi_selective_search, scored_image = run_some_deep_learning_cntk_local(
+        local_image)
+
+    # model_version = config.model_version
+
+    # upload_docdb(appliance_id, blob_url, modified_date,
+    #              modified_date_utc, usage, roi_annotated, roi_annotated_dict, roi_selective_search, model_version)
+
+    return render_template('index.html', **locals())
+
     
 @app.route("/api/uploader", methods=['POST'])
 def api_upload_file():
@@ -114,6 +142,11 @@ def api_upload_file():
     return json.dumps(scored_image)
 
 
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+    
+    
 def save_file(file):
     print(file)
 
@@ -156,6 +189,7 @@ def save_file(file):
 
     return appliance_id, blob_url, modified_date, modified_date_utc, usage
 
+    
 def upload_docdb(appliance_id, blob_url, modified_date,
     modified_date_UTC, usage, roiAnnotated, roiAnnotatedDict, roiSelectiveSearch, modelVersion):
 
@@ -193,6 +227,7 @@ def upload_docdb(appliance_id, blob_url, modified_date,
     # upload the document
     document = client.CreateDocument(collection['_self'], entry)
 
+    
 def run_some_deep_learning_cntk(pil_image):
     # Load image
     open_cv_image = np.array(pil_image)
@@ -255,4 +290,76 @@ def run_some_deep_learning_cntk(pil_image):
           
     return roi_annotated, roi_annotated_dict, roi_selective_search, urllib.parse.quote(processed_file)
 
+
+def run_some_deep_learning_cntk_local(imgPath):
+    # Load image
+    # open_cv_image = np.array(pil_image)
+    # imgOrig = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
+    # imgPath = open_cv_image
+    # Note: had to make alteration (cntk_helpers.py)
+    # imgDebug = imresize(imread(imgPath), scale)
+    # TO: imgDebug = imresize(imgPath, scale)
+    imgOrig = imread(imgPath)
+
+    currRois = computeRois(imgOrig, boAddSelectiveSearchROIs, boAddGridROIs, boFilterROIs, ss_kvals, ss_minSize,
+                           ss_max_merging_iterations, ss_nmsThreshold,
+                           roi_minDimRel, roi_maxDimRel, roi_maxImgDim, roi_maxAspectRatio, roi_minNrPixelsRel,
+                           roi_maxNrPixelsRel, grid_nrScales, grid_aspectRatios, grid_downscaleRatioPerIteration)
+
+    currRois = currRois[:cntk_nrRois]  # only keep first cntk_nrRois rois
+    imgPadded = imresizeAndPad(imgOrig, cntk_padWidth, cntk_padHeight)
+    _, _, roisCntk = getCntkInputs(imgPath, currRois, None, train_posOverlapThres, nrClasses, cntk_nrRois,
+                                   cntk_padWidth, cntk_padHeight)
+    arguments = {
+        model.arguments[0]: [np.ascontiguousarray(np.array(imgPadded, dtype=np.float32).transpose(2, 0, 1))],
+        # convert to CNTK's HWC format
+        model.arguments[1]: [np.array(roisCntk)]
+    }
+    dnnOutputs = model.eval(arguments)[0][0]
+    dnnOutputs = dnnOutputs[:len(currRois)]  # remove the zero-padded rois
+    labels, scores = scoreRois(classifier, dnnOutputs, svmWeights, svmBias, svmFeatScale, len(classes),
+                               decisionThreshold=vis_decisionThresholds[classifier])
+    # perform non-maxima surpression
+    nmsKeepIndices = []
+    if boUseNonMaximaSurpression:
+        nmsKeepIndices = applyNonMaximaSuppression(nmsThreshold, labels, scores, currRois)
+    imgDebug = visualizeResults(imgPath, labels, scores, currRois, classes,
+                                nmsKeepIndices, boDrawNegativeRois=False, boDrawNmsRejectedRois=False)
+
+    imgDebug = imgDebug[:, :, ::-1].copy()  # take care of RGB vs BGR
+    # Return processed as stream
+    ret_imgio = BytesIO()
+    imgDebug = Image.fromarray(imgDebug)
+    imgDebug.save(ret_imgio, 'PNG')
+    processed_file = base64.b64encode(ret_imgio.getvalue())
+
+    # tmp_file = os.path.join(APP_ROOT, 'images/scored.jpg')
+    # imwrite(imgDebug, tmp_file)
+    # with open(tmp_file, "rb") as image_file:
+    #    processed_file = base64.b64encode(image_file.read())
+
+    roi_selective_search = [
+        {
+            "coords": [float(r[0]), float(r[1]), float(r[2]), float(r[3])],
+            "label": {classes[int(l)]: float(s)}
+        } for l, s, r in zip(labels, scores, currRois)
+        ]
+
+    roi_annotated = [
+        {
+            "coords": [float(r[0]), float(r[1]), float(r[2]), float(r[3])],
+            "label": {classes[int(l)]: float(s)}
+        } for l, s, r, i in zip(labels, scores, currRois, range(len(labels))) if
+        (i in nmsKeepIndices) and classes[int(l)] != "__background__"
+        ]
+
+    roi_annotated_list = [classes[int(l)] for l, i in zip(labels, range(len(labels))) if
+                          (i in nmsKeepIndices) and (classes[int(l)] != "__background__")]
+    print(roi_annotated_list)
+
+    roi_annotated_dict = dict()
+    for e in roi_annotated_list:
+        roi_annotated_dict[e] = roi_annotated_dict.get(e, 0) + 1
+
+    return roi_annotated, roi_annotated_dict, roi_selective_search, urllib.parse.quote(processed_file)
 
